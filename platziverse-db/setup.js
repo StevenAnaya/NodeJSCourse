5f400a68ed47563362b05e34ab75a9be5524d2f3
StevenAnaya <stvanaya@hotmail.com>
2018-01-15 16:51:30 -0500

    Adding platziverse-mqtt module

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -6,22 +6,51 @@ const chalk = require('chalk')
 const db = require('./')
 // Creamos la instancioa del prompt para poder mostrar los mensajes por la pantalla
 const prompt = inquirer.createPromptModule()
+let autConfig = false
+
+process.argv.find((arg) => {
+  if (arg === '-y' || arg === '-yes') {
+    autConfig = true
+  }
+})
 
 async function setup () {
-  // Ahora a la funcion prompt que instanciamos le pasamos por parametro la configuracion que le queremo
-  // dar a esta confirmacion.
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: 'This will destroy your database, are you sure?'
-    }
-  ])
-  // Aqui evaluamos la respuesta, si es falsa que no ejecute el resto del script, de lo contrario qu
-  // el seteo de esta base de datos de nuevo
-  if (!answer.setup) return console.log('Nothing happened :)')
+  const config = configSetup()
 
-  const config = {
+  if (!autConfig) {
+    // Ahora a la funcion prompt que instanciamos le pasamos por parametro la configuracion que le queremo
+    // dar a esta confirmacion.
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your database, are you sure?'
+      }
+    ])
+    // Aqui evaluamos la respuesta, si es falsa que no ejecute el resto del script, de lo contrario qu
+    // el seteo de esta base de datos de nuevo
+    if (!answer.setup) return console.log('Nothing happened :)')
+
+    await db(config).catch(handleFatalError)
+    console.log('Database configured')
+    process.exit(0)
+  }
+
+  await db(config).catch(handleFatalError)
+  console.log('Database configured')
+  process.exit(0)
+}
+
+// En esta funcion capturamos el error y imprimimos el stack y luego matamos el proceso con codigo 1
+function handleFatalError (err) {
+  // Aca usamos chalk para que el error que imprimos no lo muestre con un color rojo y se pueda
+  // distinguir bien
+  console.error(`${chalk.red('[faltal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+function configSetup () {
+  return {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'steven',
     password: process.env.DB_PASS || 'zanahoria19',
@@ -36,20 +65,5 @@ async function setup () {
     // y cree una nueva. Hay que tener cuidado con este
     setup: true
   }
-  // Vamos a capturar los errores que ocurran en la configuracion de nuestra base de datos
-  await db(config).catch(handleFatalError)
-
-  console.log('Success')
-  process.exit(0)
 }
-// En esta funcion capturamos el error y imprimimos el stack y luego matamos el proceso con codigo 1
-function handleFatalError (err) {
-  // Aca usamos chalk para que el error que imprimos no lo muestre con un color rojo y se pueda
-  // distinguir bien
-  console.error(`${chalk.red('[faltal error]')} ${err.message}`)
-  console.error(err.stack)
-  process.exit(1)
-}
-
-// Aca ejecutamos de una vez la funcion cuando arranque
 setup()
